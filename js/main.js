74121d8c409e522de57930d4d8aa53a01c606c2f
JYM <114120370+juyoonmin@users.noreply.github.com>
2024-06-11 20:04:57 +0900

    swiper js 수정

--- js/main.js
+++ js/main.js
@@ -63,16 +63,20 @@ function handleLoadingScreen() {
     }
   });
 }
-var swiper = new Swiper('.swiper-container', {
-  loop: true,
-  slidesPerView: 3,
-  spaceBetween: 30,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+// 스와이퍼
+function initSwiper() {
+  new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   handleLoadingScreen();
+  initSwiper();
 });
