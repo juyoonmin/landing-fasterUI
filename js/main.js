10f6130b3d9cbbecc08524ee1a02b8a7f9289385
JYM <114120370+juyoonmin@users.noreply.github.com>
2024-06-17 16:44:53 +0900

    se3 gsap 애니메이션 완료

--- js/main.js
+++ js/main.js
@@ -94,8 +94,8 @@ function initSection2Animation() {
     scrollTrigger: {
       trigger: '.section2',
       start: 'center center',
-      end: '500%',
-      scrub: 2,
+      end: '120%',
+      // scrub: 2,
       ease: 'none',
       pin: true,
       // markers: true
@@ -108,19 +108,19 @@ function initSection2Animation() {
     .set(".se2-con2-img", { opacity: 0, x: 50 })
     .set(".se2-con3-textbox", { opacity: 0, y: -50 })
     .set(".se2-con3-textbox2", { opacity: 0, y: 50 })
-    
-    .to(".se2-con1-textbox", { opacity: 1, duration: 1, y: 0 })
-    .to(".se2-con1-img", { opacity: 1, duration: 1, y: 0 })
-    .to(".se2-con2-img", { opacity: 1, duration: 1, x: 0 })
-    .to(".se2-con3-textbox", { opacity: 1, duration: 1, y: 0 })
-    .to(".se2-con3-textbox2", { opacity: 1, duration: 1, y: 0 })
-    .to(".se2-con3-text2 p", { 
+
+    .to(".se2-con1-textbox", { opacity: 1, duration: 0.5, y: 0 })
+    .to(".se2-con1-img", { opacity: 1, duration: 0.5, y: 0 })
+    .to(".se2-con2-img", { opacity: 1, duration: 0.5, x: 0 })
+    .to(".se2-con3-textbox", { opacity: 1, duration: 0.5, y: 0 })
+    .to(".se2-con3-textbox2", { opacity: 1, duration: 0.5, y: 0 })
+    .to(".se2-con3-text2 p", {
       text: {
-        value: "our mission is to <span class='se2-red'>win</span>, and only <span class='se2-red'>win</span>. that's what we do.",
-      }, 
-      duration: 5, 
+        value: "our mission is to <span class='se2-red'>win</span>, and only <span class='se2-red'>win</span>. that's what we do<span class='blink'>.</span>",
+      },
+      duration: 2,
       ease: "none",
-      onUpdate: function() {
+      onUpdate: function () {
         const progress = this.progress(); // progress() 메서드 참조
         if (progress > 0.8) { // 진행 상태에 따라 색상 변경
           document.querySelectorAll(".se2-red").forEach(el => {
@@ -131,6 +131,15 @@ function initSection2Animation() {
         // if (progress > 0.8) {
         //   gsap.to(".se2-red", { color: "#FF0000", duration: 1 });
         // }
+      },
+      onComplete: function () {
+        gsap.to(".blink", {
+          opacity: 0,
+          duration: 0.7,
+          repeat: -1,
+          yoyo: true,
+          ease: "power2.inOut"
+        });
       }
     });
 }
@@ -138,11 +147,52 @@ function initSection2Animation() {
 // 햄버거 메뉴
 function initHamburgerMenu() {
   document.getElementById('hamburger').addEventListener('click', function () {
-      this.classList.toggle('menu_active');
-      document.getElementById('submenubox').classList.toggle('menu_active');
+    this.classList.toggle('menu_active');
+    document.getElementById('submenubox').classList.toggle('menu_active');
   });
 }
 
+// se3 애니메이션
+function initSection3Animation() {
+  const timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.section3',
+      start: 'top top',
+      end: '200%',
+      scrub: 2,
+      ease: 'none',
+      pin: true,
+      // markers: true
+    }
+  });
+
+  timeline
+    .set(".se3-main-text", { opacity: 0, x: -50 })
+    .set(".se3-red", { opacity: 0, x: -50 })
+    .set(".se3-main1", { opacity: 0, x: -50 })
+    .set(".se3-imgtext", { opacity: 0, y: -50 })
+    .set(".se3-main2", { opacity: 0, x: 50 })
+    .set(".se3-sub-maintext", { opacity: 0, x: -50 })
+    .set(".match", { opacity: 0, y: 50 })
+
+    .to(".se3-main-text", { opacity: 1, duration: 1, x: 0 })
+    .to(".se3-red", { opacity: 1, duration: 1, x: 0 })
+    .to(".se3-main1", { opacity: 1, duration: 1, x: 0 })
+    .to(".se3-imgtext", { opacity: 1, duration: 1, y: 0 })
+    .to(".se3-main2", { opacity: 1, duration: 1, x: 0, })
+    .to(".se3-sub-maintext", { opacity: 1, duration: 1, x: 0 })
+    .to(".match:first-of-type", {
+      opacity: 0.4,
+      duration: 0.5,
+      y: 0
+    })
+    .to(".match:not(:first-of-type)", {
+      opacity: 1,
+      duration: 0.5,
+      y: 0,
+      stagger: 0.5,
+    })
+}
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -150,4 +200,5 @@ document.addEventListener('DOMContentLoaded', function () {
   initSwiper();
   initSection2Animation();
   initHamburgerMenu();
+  initSection3Animation();
 });
